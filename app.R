23c494557488c56fdb03248782fd42daf078116e
Ingo Schommer <me@chillu.com>
2018-03-05 23:27:24 +1300

    Fixed merge error

--- app.R
+++ app.R
@@ -168,7 +168,6 @@ server <- function(input, output) {
     ggplot(props_filtered(), aes(decade_built, price_by_floor_area)) +
       geom_point(aes(size = price), alpha = 1/3)
   })
-  })
   
   pal <- colorNumeric(c("green", "yellow", "red"), 0:1)
   color <- function() {
